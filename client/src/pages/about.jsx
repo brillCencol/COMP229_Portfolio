75d3470e893c4b2602ec5d8bf583bd532c96ab3f
Brill John Torino <btorino@my.centennialcollege.ca>
2025-07-20 13:27:03 -0400

    Fix: update Footer import case in about.jsx for Linux deploy

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from "@/components/Navbar"
-import Footer from "@/components/footer"
+import Footer from '../components/Footer'
 import { Button } from "@/components/ui/button"
 
 export default function AboutPage() {
